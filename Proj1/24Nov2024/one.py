26e2397f3a6abe331caa65da019b71d3f4d5830e
Priti Fuse <E010846@cigniti.com>
2024-11-30 20:25:50 +0530

    30nov

---
+++ Proj1/24Nov2024/one.py
@@ -0,0 +1,18 @@
+#day 10 : exceptions
+
+print("start of prog")
+print("start of prog")
+try:
+    print("my input..",x)
+except:
+    print("=====>exception occur..")
+else:
+    print("will print when no exception occur..")
+finally:
+    print("will print always..")
+
+print("End..")
+
+
+#file handling
+
--- Proj1/New_selenium/chrome.py
+++ Proj1/New_selenium/chrome.py
@@ -9,43 +9,43 @@ from selenium.webdriver.chrome.service import Service
 driver = webdriver.Chrome()
 driver.get("https://blazedemo.com/")
 driver.maximize_window()
-title = driver.find_element(By.XPATH,"//input[@value='Find Flights']")
-# print(title.is_displayed())
-title.click()
-tbody = driver.find_element(By.XPATH,"//table[@class='table']/tbody")
-print(tbody)
-data = []
-for tr in tbody:
-    row = [item.text for item in tr.find_elements(By.XPATH,'//td[6]')]
-    data.append(row.text)
-    print(row)
-data= data[0]
-data1=[]
-for i in data:
-    okitem  = i[1:len(i)+1]
-    # print(okitem)
-    okitemf = float(okitem)
-    data1.append(okitemf)
-
-(data1.sort())
-# print(data1)
-print(data1[0])
-lowprice = str (data1[0])
-# print(lowprice)
-# print(type(lowprice))
-
+# title = driver.find_element(By.XPATH,"//input[@value='Find Flights']")
+# # print(title.is_displayed())
+# title.click()
+# tbody = driver.find_element(By.XPATH,"//table[@class='table']/tbody")
+# print(tbody)
+# data = []
 # for tr in tbody:
 #     row = [item.text for item in tr.find_elements(By.XPATH,'//td[6]')]
-#     data.append(row)
+#     data.append(row.text)
 #     print(row)
-
-
+# data= data[0]
+# data1=[]
+# for i in data:
+#     okitem  = i[1:len(i)+1]
+#     # print(okitem)
+#     okitemf = float(okitem)
+#     data1.append(okitemf)
 #
-# driver.get("https://www.yatra.com")
-# driver.maximize_window()
-# driver.implicitly_wait(4)
-# title = driver.find_element(By.XPATH,"//a[@title='Multicity']")
-# print(title.is_displayed())
-# title.click()
+# (data1.sort())
+# # print(data1)
+# print(data1[0])
+# lowprice = str (data1[0])
+# # print(lowprice)
+# # print(type(lowprice))
+#
+# # for tr in tbody:
+# #     row = [item.text for item in tr.find_elements(By.XPATH,'//td[6]')]
+# #     data.append(row)
+# #     print(row)
+#
+#
+# #
+# # driver.get("https://www.yatra.com")
+# # driver.maximize_window()
+# # driver.implicitly_wait(4)
+# # title = driver.find_element(By.XPATH,"//a[@title='Multicity']")
+# # print(title.is_displayed())
+# # title.click()
 time.sleep(10)
 print("Done...!")
