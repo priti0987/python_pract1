838f99c8478597a51f9bc80f5cee00f7d1a7e8f9
Priti Fuse <E010846@cigniti.com>
2024-12-13 16:51:18 +0530

    13dec

--- Proj1/New_selenium/3dec.py
+++ Proj1/New_selenium/3dec.py
@@ -2,19 +2,22 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-
+#selenium4 ... updated on 13dec2024
 driver = webdriver.Chrome()
 url = "https://opensource-demo.orangehrmlive.com/"
-#driver.get("https://blazedemo.com/")
 driver.get(url)
 time.sleep(7)
-
 driver.maximize_window()
+driver.find_element(By.XPATH, "//input[@name='username']").send_keys("Admin")
+driver.find_element(By.XPATH, "//input[@name='password']").send_keys("admin123")
+driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
-driver.find_element(By.XPATH,"//input[@name='username']").send_keys("Admin")
-
-
+act_title = driver.title
+exp_title = 'OrangeHRM'
+if act_title == exp_title:
+    print("Login Test Pass")
+else:
+    print("Login Test Failed")
 time.sleep(10)
 print("done....")
+
---
+++ Proj1/New_selenium/selenium_4.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.chrome.service import service
+
+#serv_obj = service(location of executable path)
+# driver = webdriver.Chrome(service = serv_obj)
+
+#driver.get(_url)
